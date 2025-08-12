ae9184c9d7202077c547a2e6c8b3a43975abd2c9
sandythosh <98731125+sandythosh@users.noreply.github.com>
2025-08-12 10:03:29 +0530

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,4 +1,10 @@
-
 def calculateStats(numbers):
   # implement the computation of statistics here and return the results
-  return None
+  avg = sum(numbers) / len(numbers) if numbers else float('nan')
+  min_val = min(numbers) if numbers else float('nan')
+  max_val = max(numbers) if numbers else float('nan')
+  return {
+      "avg": avg,
+      "min": min_val,
+      "max": max_val
+  }
